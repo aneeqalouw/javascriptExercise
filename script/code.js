04a9e0d60f852105c698ab235f0db58303ef742a
aneeqalouw <aneeqalouw057@gmail.com>
2023-10-10 13:42:13 +0200

    folder structure

---
+++ script/code.js
@@ -0,0 +1,4 @@
+let hours = parseInt(prompt("How many hours did you work?"))
+let rate = parseInt(prompt("What is your hourly rate?")) 
+let salary = hours * rate
+console.log(`Your salary is R${salary}`) 
